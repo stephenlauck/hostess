3f35d23d4dbaff14f0cebf3e27896b216c69aa2d
Stephen Lauck <stephen.lauck@akqa.com>
2012-02-27 15:37:16 -0800

    added list and show functions for cli / hostess

--- lib/hostess/cli.rb
+++ lib/hostess/cli.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 
+# assumes rackspace credentials for CRUD load balancers
+
+# hostess --new 'name of load balancer' --port 80 --node-search '^node-regex' --node-port 80
+# hostess --list 
+# hostest --show 'name of load balancer'
+# hostess --delete 'name of load balancer'
+# hostess --add-node 'name of load balancer' --node-ip '10.x.x.x' --node-port 80
+# hostess --delete-node 'name of load balancer' --node-ip '10.x.x.x'
+# hostess --sync-nodes 'name of load balancer' --node-search '^node-regex' --node-port 80
+
+# Hostess::CLI.run(ARGV)
+
+
 require 'thor'
 require 'fog'
+require 'pp'
 
 module Hostess
   class CLI < Thor
@@ -19,12 +33,19 @@ module Hostess
 
     desc "list", "List all load balancers"
     def list
+      puts "id \t name"
+      puts "-------------------------------"
       @load_balancers.list_load_balancers.body['loadBalancers'].each do |lb|
-        puts "#{lb['name']}"
+        puts "#{lb['id']}\t #{lb['name']}"
       end
     end
 
-  end
+    desc "show", "Show a load balancer by ID"
+    def show(load_balancer_id)
+      pp @load_balancers.get_load_balancer(load_balancer_id)
+    end
 
+
+  end
 end
 
