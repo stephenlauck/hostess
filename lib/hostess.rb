6677b0e6615a19ed4f4373a7a00692f8ffb103d7
Stephen Lauck <stephen.lauck@akqa.com>
2012-02-27 15:37:01 -0800

    removed initialize and put it in cli.rb

--- lib/hostess.rb
+++ lib/hostess.rb
@@ -5,13 +5,5 @@ require "hostess/version"
 module Hostess
   autoload :CLI, "hostess/cli"
 
-  def initialize
-    @connection = Fog::Rackspace
-
-    @load_balancers = Fog::Rackspace::LoadBalancers.new(
-      :rackspace_auth_url    => 'auth.api.rackspacecloud.com',
-      :rackspace_lb_endpoint => Fog::Rackspace::LoadBalancers::ORD_ENDPOINT
-    )
-  end
 
 end
