befccc71c5a9d4c51bf212dd20aa93a33a12950c
Stephen Lauck <stephen.lauck@akqa.com>
2012-03-07 16:13:09 -0800

    added parameter for datacenter

--- lib/chef/knife/rackspace_loadbalancer_base.rb
+++ lib/chef/knife/rackspace_loadbalancer_base.rb
@@ -3,7 +3,8 @@ module RackspaceLoadbalancerBase
     @loadbalancers ||= begin
       Hostess::LoadBalancer.new(
         Chef::Config[:knife][:rackspace_username],
-        Chef::Config[:knife][:rackspace_api_key]
+        Chef::Config[:knife][:rackspace_api_key],
+        Chef::Config[:knife][:rackspace_datacenter]
       )
     end
   end
